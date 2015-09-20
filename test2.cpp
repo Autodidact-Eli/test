b7c3bb6f056fb1ef1c459cd419ecb8367adf58a1
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-20 16:35:25 -0400

    modified test2.cpp and test3.cpp

--- test2.cpp
+++ test2.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
 	cout << "2nd test file here.\n";
+	cout << "Changes to 2nd.\n";
 	return 0;
 }
--- test3.cpp
+++ test3.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
 	cout << "3rd file here.\n";
+	cout << "Changes to 3rd.\n";
 	return 0;
 }
